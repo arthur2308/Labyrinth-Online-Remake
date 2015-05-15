d25ba4ea178fd1bb89b71c6e8a63dfb74def34d9
MICAH TIMOTHY KELLY <poptarts@uw1-320-template>
2015-05-15 16:37:10 -0700

    New client code for testing!

--- client/GameClient.js
+++ client/GameClient.js
@@ -2,6 +2,19 @@
 /*globals
   $, chrome, document, DataView, TextEncoder, TextDecoder
 */
+const VALID_PLAY_TINT = '#696969',
+      BOARD_WIDTH = 9;
+var _canvas,
+    _stage,
+    _img,
+    _pieceWidth,
+    _pieceHeight,
+    _boardWidth,
+    _boardHeight,
+    _currentPiece,
+    _previousLoc,
+    _mouse;
+
 
 var socks = chrome.sockets.tcp,
   socketId = '',
@@ -34,6 +47,10 @@ $(document).ready(function () {
   socks.onReceiveError.addListener(function (info) {
     log("ReceiveError " + JSON.stringify(info));
   });
+
+  _img = new Image();
+  _img.addEventListener('load',onImage,false);
+  _img.src = "bkgrd.png";
 
   $('#connect').click(function () {
     var server = $('#server').val();
@@ -85,3 +102,72 @@ $(document).ready(function () {
     });
   });
 });
+
+function onImage(e) {
+  "use strict";
+
+  _pieceWidth = Math.floor(_img.width / BOARD_WIDTH);
+  _pieceHeight = Math.floor(_img.height / BOARD_WIDTH);
+  _boardWidth = _pieceWidth * BOARD_WIDTH;
+  _boardHeight = _pieceHeight * BOARD_HEIGHT;
+  setCanvas();
+  initBoard();
+}
+
+function setCanvas() {
+  "use strict";
+
+  _canvas = document.getElementById('board');
+  _stage = _canvas.getContext('2d');
+  _canvas.width = _boardWidth;
+  _canvas.height = _boardHeight;
+  _canvas.style.border = "1px solid black";
+}
+
+function initBoard() {
+  "use strict";
+
+  _mouse = {x:0,y:0};
+  _stage.drawImage(_img, 0, 0, _boardWidth, _boardHeight, 0, 0, _boardWidth, _boardHeight);
+  buildTable();
+}
+
+function buildTable() {
+  "use strict";
+
+  var i,
+    piece,
+    xPos = 0,
+    yPos = 0;
+
+  for (i = 0; i < BOARD_WIDTH * BOARD_WIDTH; i += 1) {
+    piece = new Image();
+    if (xPos == 0 || yPos == 0 || xPos / _pieceWidth == BOARD_WIDTH-1 || yPos / _pieceHeight == BOARD_WIDTH-1) {
+      piece.src = "bkgrd.png";
+    }
+    else {
+      piece.src = "angle-d-l.png";
+    }
+    _stage.drawImage(piece, xPos, yPos, _pieceWidth, _pieceHeight);
+    _stage.strokeRect(xPos, yPos, _pieceWidth, _pieceHeight);
+    xPos += _pieceWidth;
+    if (xPos >= _boardWidth) {
+      xPos = 0;
+      yPos += _pieceHeight;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
